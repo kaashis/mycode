81efbbcf7038818980a77dca9a4ce6d52dd011d3
Aashis Karki <aashiskarki@my.unt.edu>
2021-05-12 14:48:57 +0000

    learning about dictionaries

--- custif/your_career.py
+++ custif/your_career.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
+import os
+clear = lambda: os.system('clear')
+
 spending_list =["vacation", "charity", "stocks"]
 desert_island_list = ["books", "grill", "buddy"]
+
+clear()
+print("******** Welcome to your career guide program********* \n")
+print("Please follow the prompts to proceed.\n")
 
 print("What would you to spend your tax refund on?")
 print(spending_list)
 spending = input(">").lower()
 
-print("What would you bring on a desert island?")
+print("\nWhat would you bring on a desert island?")
 print(desert_island_list)
 desert_island = input(">").lower()
-
+print()
 
 if spending =="vacation":
     if desert_island == "books":
