79acf16727cff7101850dc036974b37173506157
Aashis Karki <aashiskarki@my.unt.edu>
2021-05-13 22:27:09 +0000

    fine tuned2

--- function_challenge/grab_bag.py
+++ function_challenge/grab_bag.py
@@ -31,9 +31,9 @@ def prompter():
         prompter()
 
 def max3():
-    num1=int(input("Please enter the first number: \n >"))
-    num2=int(input("Please enter the first number: \n >"))
-    num3=int(input("Please enter the first number: \n >"))
+    num1=int(input("Please enter the first  number: "))
+    num2=int(input("Please enter the second number: "))
+    num3=int(input("Please enter the third  number: "))
             
     def max_of_three(a,b,c):
         max=a
@@ -82,14 +82,13 @@ def productList():
 
 def reverseString():
     string= input("Enter the string that you would like to reverse:\n >")
-    def reverse_string(string):
-        x=len(string)-1
-        print("\nHere is your reversed string: ",end="")
-        while x>=0:
-            print(string[x],end="")
-            x -= 1
-        print()
-    reverse_string(string)
+    
+    x=len(string)-1
+    print("\nHere is your reversed string: ",end="")
+    while x>=0:
+        print(string[x],end="")
+        x -= 1
+    print()
 
 def looper():
     prompter()
